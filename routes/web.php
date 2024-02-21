ae8bc6e53b59582645c78d1046bfd9d11b6f059a
EdoBergamo <edo.bergamo03@icloud.com>
2024-02-21 08:26:14 -0800

    fix: removed first dashboard route handling

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('admin/projects', ProjectController::class);
