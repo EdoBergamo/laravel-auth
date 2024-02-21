5849b97e958ce83c7728368cef5cecd741715432
EdoBergamo <edo.bergamo03@icloud.com>
2024-02-21 07:02:30 -0800

    fix: resolved some issues

--- database/seeders/ProjectsTableSeeder.php
+++ database/seeders/ProjectsTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,25 @@ class ProjectsTableSeeder extends Seeder
      */
     public function run()
     {
-        
+        Project::create([
+            'title' => 'Progetto 1',
+            'description' => 'Descrizione Progetto 1'
+        ]);
+        Project::create([
+            'title' => 'Progetto 2',
+            'description' => 'Descrizione Progetto 2'
+        ]);
+        Project::create([
+            'title' => 'Progetto 3',
+            'description' => 'Descrizione Progetto 3'
+        ]);
+        Project::create([
+            'title' => 'Progetto 4',
+            'description' => 'Descrizione Progetto 4'
+        ]);
+        Project::create([
+            'title' => 'Progetto 5',
+            'description' => 'Descrizione Progetto 5'
+        ]);
     }
 }
